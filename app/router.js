178e3abfdf7b50e549ed61715ed07f046ef0a6b8
Billy Chan <ccw.billy.123@gmail.com>
2024-07-30 17:58:25 +0800

    WIP

--- app/router.js
+++ app/router.js
@@ -11,37 +11,37 @@ Router.map(function () {
   this.route('crates');
   this.route('crate', { path: '/crates/:crate_id' }, function () {
     this.route('articles');
-    this.route('versions');
-    this.route('dependencies');
-    this.route('version', { path: '/:version_num' });
-    this.route('version-dependencies', { path: '/:version_num/dependencies' });
-    this.route('range', { path: '/range/:range' });
+    // this.route('versions');
+    // this.route('dependencies');
+    // this.route('version', { path: '/:version_num' });
+    // this.route('version-dependencies', { path: '/:version_num/dependencies' });
+    // this.route('range', { path: '/range/:range' });
 
-    this.route('reverse-dependencies', { path: 'reverse_dependencies' });
+    // this.route('reverse-dependencies', { path: 'reverse_dependencies' });
 
-    this.route('owners');
-    this.route('settings');
+    // this.route('owners');
+    // this.route('settings');
 
     // Well-known routes
-    this.route('docs');
-    this.route('repo');
+    // this.route('docs');
+    // this.route('repo');
   });
-  this.route('me', function () {
-    this.route('crates');
-    this.route('following');
-    this.route('pending-invites');
-  });
-  this.route('settings', function () {
-    this.route('email-notifications');
-    this.route('profile');
-    this.route('tokens', function () {
-      this.route('new');
-    });
-  });
+  // this.route('me', function () {
+  //   this.route('crates');
+  //   this.route('following');
+  //   this.route('pending-invites');
+  // });
+  // this.route('settings', function () {
+  //   this.route('email-notifications');
+  //   this.route('profile');
+  //   this.route('tokens', function () {
+  //     this.route('new');
+  //   });
+  // });
   this.route('user', { path: '/users/:user_id' });
-  this.route('install');
+  // this.route('install');
   this.route('search');
-  this.route('dashboard');
+  // this.route('dashboard');
   this.route('keywords');
   this.route('keyword', { path: '/keywords/:keyword_id' }, function () {
     this.route('index', { path: '/' });
@@ -50,17 +50,17 @@ Router.map(function () {
   this.route('category', { path: '/categories/:category_id' }, function () {
     this.route('index', { path: '/' });
   });
-  this.route('category-slugs', { path: 'category_slugs' });
-  this.route('team', { path: '/teams/:team_id' });
-  this.route('policies', function () {
-    this.route('security');
-  });
+  // this.route('category-slugs', { path: 'category_slugs' });
+  // this.route('team', { path: '/teams/:team_id' });
+  // this.route('policies', function () {
+  //   this.route('security');
+  // });
   // The canonical security page is `/policies/security`; this is a redirect to support guessing
   // of the URL to be `/security`.
-  this.route('security');
-  this.route('data-access');
-  this.route('confirm', { path: '/confirm/:email_token' });
-  this.route('accept-invite', { path: '/accept-invite/:token' });
+  // this.route('security');
+  // this.route('data-access');
+  // this.route('confirm', { path: '/confirm/:email_token' });
+  // this.route('accept-invite', { path: '/accept-invite/:token' });
 
   this.route('catch-all', { path: '*path' });
 });
