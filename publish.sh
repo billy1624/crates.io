18390a30457be6f87f361732ad6bdb3c22827ba9
Billy Chan <ccw.billy.123@gmail.com>
2024-08-12 15:19:56 +0800

    Fix

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-# cp dist/index.html dist/404.html
+cp dist/index.html dist/404.html
 
 cd ~/Projects/billy1624.github.io
 find -not -path "./.git/*" -not -name ".git" -not -name ".gitignore" -delete
