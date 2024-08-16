a8a187b4226c5631e7a255863cf82447b0f6de4d
Billy Chan <ccw.billy.123@gmail.com>
2024-08-16 09:28:09 +0800

    WIP

--- related-articles/src/main.rs
+++ related-articles/src/main.rs
@@ -72,11 +72,11 @@ fn gen_sitemap() -> Result<(), Box<dyn Error>> {
         dbg!(&page_num);
 
         sitemap_crates.push(Sitemap::new(
-            format!("https://rustacean.info/sitemap-crates-{page_num:03}.xml"),
+            format!("https://rustacean.info/sitemap-crates-{page_num:03}.xml.gz"),
             None,
         ));
         sitemap_articles.push(Sitemap::new(
-            format!("https://rustacean.info/sitemap-articles-{page_num:03}.xml"),
+            format!("https://rustacean.info/sitemap-articles-{page_num:03}.xml.gz"),
             None,
         ));
 
